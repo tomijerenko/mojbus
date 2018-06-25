9fcbcf9d49c503bbe27cb26bace5130f86c49638
1tomy1 <tomi1995.jerenko@gmail.com>
2018-06-25 14:54:40 +0200

    Added endpoints for routes

---
+++ MojBus/Controllers/MojBusApiController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MojBus.Data;
+using MojBus.Helpers;
+
+namespace MojBus.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[action]")]
+    public class MojBusApiController : Controller
+    {
+        MojBusContext _context;
+
+        public MojBusApiController(MojBusContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Stops()
+        {
+            return Json(MojBusHelper.GetStops(_context));
+        }
+
+        public IActionResult Routes()
+        {
+            return Json(MojBusHelper.GetRoutes(_context));
+        }
+
+        [HttpGet]
+        public IActionResult StopData(int stopId)
+        {
+            return Json(MojBusHelper.StopTimesForStop(_context, stopId));
+        }
+
+        [HttpGet]
+        public IActionResult StopDataForRoute(int stopId, int routeId)
+        {
+            return Json(MojBusHelper.StopTimesForStop(_context, stopId, routeId));
+        }
+    }
+}
